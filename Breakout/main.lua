e5170785e3da77e1f9d5138552d5544bf131ddcb
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-12-12 09:20:48 +0800

    Update main.lua

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -23,6 +23,7 @@ function love.load()
     }
     
     gFrames = {
+        ['arrows'] = GenerateQuads(gTextures['arrows'], 24, 24),
         ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
         ['balls'] = GenerateQuadsBalls(gTextures['main']),
         ['bricks'] = GenerateQuadsBricks(gTextures['main'])
@@ -57,10 +58,19 @@ function love.load()
         ['start'] = function() return StartState() end,
         ['play'] = function() return PlayState() end
         ['serve'] = function() return ServeState() end,
-        ['game-over'] = function() return GameOverState() end
+        ['game-over'] = function() return GameOverState() end,
+        ['victory'] = function() return VictoryState() end,
+        ['high-scores'] = function() return HighScoreState() end,
+        ['enter-high-score'] = function() return EnterHighScoreState() end,
+        ['paddle-select'] = function() return PaddleSelectState() end
     }
-    gStateMachine:change('start')
+    gStateMachine:change('start'{
+        highScores = loadHighScores()
+    })
 
+    gSounds['music']:play()
+    gSounds['music']:setLooping(true)
+    
     love.keyboard.keysPressed = {}
 end
 
@@ -103,6 +113,46 @@ function love.draw()
     push:apply('end')
 end
 
+function loadHighScores()
+    love.filesystem.setIdentity('breakout')
+    
+    if not love.filesystem.exists('breakout.lst') then
+        local scores = ''
+        for i = 10, 1, -1 do
+            scores = scores .. 'CTO\n'
+            scores = scores .. tostring(i * 1000) .. '\n'
+        end
+
+        love.filesystem.write('breakout.lst', scores)
+    end
+    
+    local name = true
+    local currentName = nil
+    local counter = 1
+    
+    local scores = {}
+
+    for i = 1, 10 do
+        scores[i] = {
+            name = nil,
+            score = nil
+        }
+    end
+    
+    for line in love.filesystem.lines('breakout.lst') do
+        if name then
+            scores[counter].name = string.sub(line, 1, 3)
+        else
+            scores[counter].score = tonumber(line)
+            counter = counter + 1
+        end
+        name = not name
+    end
+
+    return scores
+end
+        
+    
 function renderHealth(health)
     local healthX = VIRTUAL_WIDTH - 100
     for i = 1, health do
