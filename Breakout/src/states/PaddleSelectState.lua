23a9386ef7bb123a2de71a72516c506d6092e17d
ajrcastillo <72683014+ajrcastillo@users.noreply.github.com>
2020-12-18 21:03:29 +0800

    Update PaddleSelectState.lua

--- Breakout/src/states/PaddleSelectState.lua
+++ Breakout/src/states/PaddleSelectState.lua
@@ -35,7 +35,7 @@ function PaddleSelectState:update(dt)
             score = 0,
             highScores = self.highScores,
             level = 1,
-            recoverPoints = 5000
+            recoverPoints = 5000,
             paddlePoints = 7000,
             keys = 3 
         })
